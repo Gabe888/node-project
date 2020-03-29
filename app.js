1fbda459a3e27a822bee626d26784835557a98ce
GS888 <gsnydermail@gmail.com>
2020-03-29 15:05:15 -0400

    saving all work

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-var express = require('express');
-var app = express();
+    var express = require('express');
+    var app = express();
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
+    var gdata = require('./googlesheet');
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Now listening at http://%s:%s", host, port)
-})
+// set up server
+    // app.get('/', function (req, res) {
+    // res.send('Hello World');
+    // })
+
+    // var server = app.listen(8081, function () {
+    // var host = server.address().address
+    // var port = server.address().port
+    
+    // console.log("Now listening at http://%s:%s", host, port)
+    // })
+
+// every day at 7:30ET
+    // Cron will run this program
+// Get pushup data from google sheets 
+    console.log(gdata);
+
+// Put that info into an email or text Message
+// send email or text message
